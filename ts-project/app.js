fc39906ab8c60ac43af7943c6af165ff02e71f37
Yuri Khandros <yvhown@gmail.com>
2023-02-12 21:09:56 +0200

    120223-2part

--- ts-project/app.js
+++ ts-project/app.js
@@ -5,4 +5,6 @@ var _loop_1 = function (i) {
 for (var i = 0; i < 3; i++) {
     _loop_1(i);
 }
+var a = 10;
+console.log("a = ".concat(a));
 //# sourceMappingURL=app.js.map
--- ts-project/app.ts
+++ ts-project/app.ts
@@ -1,3 +1,5 @@
 for (let i=0; i<3; i++) {
     setTimeout(()=> console.log(i));
 }
+let a=10;
+console.log(`a = ${a}`);

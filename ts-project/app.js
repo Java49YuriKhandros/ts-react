dc60ada2b9a4bdab9eba6bb63f4019f9762167dc
Yuri Khandros <yvhown@gmail.com>
2023-02-12 21:10:40 +0200

    120223-2part

--- ts-project/app.js
+++ ts-project/app.js
@@ -1,10 +1,7 @@
 "use strict";
-var _loop_1 = function (i) {
-    setTimeout(function () { return console.log(i); });
-};
-for (var i = 0; i < 3; i++) {
-    _loop_1(i);
-}
-var a = 10;
-console.log("a = ".concat(a));
+// for (let i=0; i<3; i++) {
+//     setTimeout(()=> console.log(i));
+// }
+// let a=10;
+// console.log(`a = ${a}`);
 //# sourceMappingURL=app.js.map
